0980941df8343ee9a3a0ddc9a19a22aa32855be7
proniraj <ntnirajthakur21@gmail.com>
2026-09-15 21:35:14 +0545

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
+    let shell_builtin_commands = ["echo", "exit", "type"];
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -14,9 +16,22 @@ fn main() -> io::Result<()> {
         if command.trim() == "exit" {
             break;
         } else if command.trim().starts_with("echo") {
-            print!("{}\n", &command.trim()[5..]);
+            println!("{}", &command.trim()[5..]);
+        } else if command.trim().starts_with("type") {
+            let mut found: bool = false;
+            for shell_builtin_command in shell_builtin_commands {
+                if shell_builtin_command == &command.trim()[5..] {
+                    found = true;
+                    break;
+                }
+            }
+            if found {
+                println!("{} is a shell builtin", &command.trim()[5..]);
+            } else {
+                println!("{}: not found", &command.trim()[5..]);
+            }
         } else {
-            print!("{}: command not found\n", command.trim());
+            println!("{}: command not found", command.trim());
         }
     }
 
