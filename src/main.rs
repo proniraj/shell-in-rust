56aaa163f2df6aaba3422e2a10d02026a4d8c81e
proniraj <ntnirajthakur21@gmail.com>
2026-09-15 22:19:22 +0545

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
 use std::io::{self, Write};
 
 fn main() -> io::Result<()> {
-    let shell_builtin_commands = ["echo", "exit", "type"];
-
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
+        let mut user_input = String::new();
 
         io::stdin()
-            .read_line(&mut command)
+            .read_line(&mut user_input)
             .expect("Failed to read line");
 
-        if command.trim() == "exit" {
-            break;
-        } else if command.trim().starts_with("echo") {
-            println!("{}", &command.trim()[5..]);
-        } else if command.trim().starts_with("type") {
-            let mut found: bool = false;
-            for shell_builtin_command in shell_builtin_commands {
-                if shell_builtin_command == &command.trim()[5..] {
-                    found = true;
-                    break;
-                }
-            }
-            if found {
-                println!("{} is a shell builtin", &command.trim()[5..]);
-            } else {
-                println!("{}: not found", &command.trim()[5..]);
-            }
-        } else {
-            println!("{}: command not found", command.trim());
+        let full_command: Vec<&str> = user_input.split_whitespace().collect();
+
+        if full_command.len() < 1 {
+            continue;
+        }
+
+        match full_command.as_slice() {
+            ["exit"] => std::process::exit(0),
+            ["echo", rest @ ..] => println!("{}", rest.join(" ")),
+            ["type", rest @ ("echo" | "exit" | "type")] => println!("{} is a shell builtin", rest),
+            ["type", rest @ ..] => println!("{}: not found", rest.join(" ")),
+            _ => println!("{}: command not found", full_command[0]),
         }
     }
 
-    Ok(())
+    // Ok(())
 }
