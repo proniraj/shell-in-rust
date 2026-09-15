68a32d7c4e608ec805f9ee0cb4112546221ac82a
proniraj <ntnirajthakur21@gmail.com>
2026-09-15 20:52:36 +0545

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+fn main() -> io::Result<()> {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
+
+    print!("{}: command not found", command.trim());
+
+    Ok(())
 }
