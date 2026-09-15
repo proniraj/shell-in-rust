f7bc53bf6d220256cef275b6268e2a827ba805af
proniraj <ntnirajthakur21@gmail.com>
2026-09-15 21:04:00 +0545

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 use std::io::{self, Write};
 
-// -> io::Result<()>
-fn main() {
+fn main() -> io::Result<()> {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
 
-        if command.to_string() == "exit" {
-            break;
-        }
-
         io::stdin()
             .read_line(&mut command)
             .expect("Failed to read line");
 
+        if command.trim() == "exit" {
+            break;
+        }
+
         print!("{}: command not found\n", command.trim());
     }
 
-    // Ok(())
+    Ok(())
 }
