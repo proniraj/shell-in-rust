7ec20b7c7f8883b39328f869ab14e78bcfcbae4c
proniraj <ntnirajthakur21@gmail.com>
2026-09-16 13:09:46 +0545

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::{self};
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn find_executable(command: &str) -> Option<PathBuf> {
     match env::var_os("PATH") {
@@ -44,6 +45,31 @@ fn type_command(arguments: &[&str]) {
     }
 }
 
+fn run_external_command(command_with_args: &[&str]) {
+    match command_with_args {
+        [command, args @ ..] => match find_executable(command) {
+            Some(_) => {
+                let mut cmd = Command::new(command);
+
+                cmd.args(args);
+
+                match cmd.output() {
+                    Ok(result) => {
+                        if result.status.success() {
+                            print!("{}", String::from_utf8_lossy(&result.stdout));
+                        } else {
+                            eprint!("{}", String::from_utf8_lossy(&result.stderr));
+                        }
+                    }
+                    Err(error) => eprintln!("Error running command: {}", error),
+                }
+            }
+            None => println!("{}: command not found", command),
+        },
+        _ => (),
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -62,7 +88,7 @@ fn main() {
             ["exit"] => std::process::exit(0),
             ["echo", rest @ ..] => println!("{}", rest.join(" ")),
             ["type", rest @ ..] => type_command(rest),
-            _ => println!("{}: command not found", full_command[0]),
+            external_command => run_external_command(external_command),
         }
     }
 }
